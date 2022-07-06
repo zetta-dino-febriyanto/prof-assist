99e54c60fdd1bdd9c307a8bd5f580908d5d810fd
Zetta Eric Julianto <algonacci1@gmail.com>
2022-07-06 08:48:34 +0700

    add response on profassist

--- Flask/app.py
+++ Flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -11,6 +11,14 @@ def index():
 @app.route('/prof-assist')
 def prof_assist():
     return render_template('prof-assist.html')
+
+def chatbot_response(msg):
+    return "Hello, I'm ProfAssist. The Teacher's Digital Assistant for Students"
+
+@app.route("/get")
+def get_prof_assist_response():
+    query = request.args.get('msg')
+    return chatbot_response(query)
 
 if __name__ == '__main__':
     app.run(debug=True)
