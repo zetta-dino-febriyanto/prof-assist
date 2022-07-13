94c0c002851db4a744f62e895c13ed7808043a83
Dino Febriyanto <44940977+alien087@users.noreply.github.com>
2022-07-13 09:36:19 +0700

    add documentation on module.py

--- Flask/module.py
+++ Flask/module.py
@@ -10,42 +10,101 @@ import time
 
 
 def preprocessing(file_path):
+    '''
+    This function is to Extract Text from PDF document
+    and Preprocessing it
+
+    :param str file_path: Path the PDF file
+    '''
+    #Convert PDF to Text
     pdf_converter = PDFToTextConverter(
         remove_numeric_tables=True, valid_languages=["en"])
     converted = pdf_converter.convert(file_path=file_path, meta={
                                       "company": "Company_1", "processed": False})
+
+    #Preprocess the text and save it to preprocessor object
     preprocessor = PreProcessor(
         split_by="word", split_length=200, split_overlap=10)
+    
     preprocessed = preprocessor.process(converted)
+
+    #Return The 
     return preprocessed
 
 
 def document_store(document_preprocessed):
+    '''
+    This function is to store document that has been preprocessed
+    to the document store
+
+    :param object document_preprocessed: Document that has been preprocessed
+    '''
+    #Initialization of Document Store
     document_store = FAISSDocumentStore(
         faiss_index_factory_str="Flat", return_embedding=True,)
+
+    #Delete all Documents on Document Store (to make sure is the document store is empty)
     document_store.delete_all_documents()
+
+    #Store Preprocessed Text to out Document Store
     document_store.write_documents(document_preprocessed)
+
+    #Return the Document Store
     return document_store
 
 
 def question_answer_pipeline(document_store):
+    '''
+    This function is to make question answering pipeline
+
+    :param object document_store: The document store
+    '''
+    #Initialize the Retriever
     retriever = DensePassageRetriever(document_store=document_store)
+
+    #Initialize the Reader
     reader = FARMReader(
         model_name_or_path='deepset/tinyroberta-squad2', use_gpu=True)
+    
+    #Update embeddings on document store using Retriever
     document_store.update_embeddings(retriever)
+
+    #Initialize the Pipeline using Reader and Retriever
     pipeline = ExtractiveQAPipeline(reader, retriever)
+
+    #Return the pipeline
     return pipeline
 
 
 def question_generator_pipeline(document_store):
+    '''
+    This function is to make question generation pipeline
+
+    :param object document_store: The document store
+    '''
+    #Initialize the Reader
     reader = FARMReader(
         model_name_or_path='deepset/tinyroberta-squad2', use_gpu=True)
+
+    #Initialize Question Generator Object
     question_generator = QuestionGenerator()
+
+    #Initialize Question Generator Pipeline using question generator object and reader
     qag_pipeline = QuestionAnswerGenerationPipeline(question_generator, reader)
+
+    #Return the question generator Pipeline
     return qag_pipeline
 
 
 def question_generator(document_store, qag_pipeline):
+    '''
+    This function is to perform question generator (to generate question from document on document store)
+    and save it to excel
+
+    :param object document_store: The document store
+    :param object qag_pipeline: Question generator pipeline
+    '''
+    #Initialize Variable
     row = 1
     column = 0
     timestr = time.strftime("%Y%m%d-%H%M%S")
